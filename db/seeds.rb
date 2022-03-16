64c5556dcfade38ba608c0d564992f04d23c62fc
Kurt Pili <kurtpili3@gmail.com>
2022-03-16 12:43:46 -0500

    Updated seed file to delete database entries & reset primary keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# Reset db
+Customer.delete_all
+AdminUser.delete_all
+
+# Reset primary keys
+Customer.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME= 'customers'")
+AdminUser.connection.execute("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME= 'admin_users'")
+
+# Create admin user
+if Rails.env.development?
+  AdminUser.create!(email: "admin@example.com", password: "password",
+                    password_confirmation: "password")
+end
