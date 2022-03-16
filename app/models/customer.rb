a89d286fc3eb4e2eee4afffbec6ceaa5a4170a32
Kurt Pili <kurtpili3@gmail.com>
2022-03-16 14:38:31 -0500

    Added Validation to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+  # Validation
+  validates :full_name, presence: true
+  validates :phone_number, length: { minimum: 10 }
+  validates :email_address, with: Devise.email_regexp
 end
